285dbdbd2621067d0136acffe6a4c5a9bb99deb0
nadiramlha <88009253+nadiramlha@users.noreply.github.com>
2023-04-03 20:39:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -45,51 +45,6 @@ app.get("/data/:id", (req, res) => {
     }
 });
 
-// POST new data
-app.post("/data", (req, res) => {
-    const { name, origin, role } = req.body;
-    const newData = { name, origin, role };
-    db.push(newData);
-    res.status(201).json({
-        message: "Data created successfully",
-        data: newData,
-    });
-});
-
-// PUT update data by id
-app.put("/data/:id", (req, res) => {
-    const { id } = req.params;
-    const { name, origin, role } = req.body;
-    const index = db.findIndex(d => d.name === id);
-    if (index !== -1) {
-        db[index] = { name, origin, role };
-        res.status(200).json({
-            message: "Data updated successfully",
-            data: db[index],
-        });
-    } else {
-        res.status(404).json({
-            message: "Data not found",
-        });
-    }
-});
-
-// DELETE data by id
-app.delete("/data/:id", (req, res) => {
-    const { id } = req.params;
-    const index = db.findIndex(d => d.name === id);
-    if (index !== -1) {
-        db.splice(index, 1);
-        res.status(200).json({
-            message: "Data deleted successfully",
-        });
-    } else {
-        res.status(404).json({
-            message: "Data not found",
-        });
-    }
-});
-
 app.listen(port, () =>{
     console.log('Server is running on port '+port);
 });
